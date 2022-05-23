bd697e4307a0e737a70b7ac449ce024d7a8fccab
junk dog <omaeadmi0909@gmail.com>
2022-05-23 19:55:41 +0900

    style: Todo コンポーネントに TodoCard 追加

--- src/component/project/Todo/Todo.tsx
+++ src/component/project/Todo/Todo.tsx
@@ -1,10 +1,14 @@
 import { CreateModal } from '../CreateModal';
+import { TodoCard, CompleteCard } from '../TodoCard';
 import { TodoProps } from './Todo.type';
+
 export const baseId = 'project-todo';
 
 export const Todo: React.FC<TodoProps> = ({ title }) => (
   <div className='p-7'>
     <h2 className='text-lg font-bold text-primary-800'>{title}</h2>
+    <TodoCard />
+    <CompleteCard />
     <CreateModal />
   </div>
 );
--- src/model/modal.ts
+++ src/model/modal.ts
@@ -2,5 +2,5 @@ import { atom } from 'recoil';
 
 export const createState = atom<boolean>({
   key: 'createState',
-  default: true,
+  default: false,
 });
